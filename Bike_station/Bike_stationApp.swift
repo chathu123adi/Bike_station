314a25b72960f508b94c6d97b505691617e632c8
Chathuranga Adikari <chathuranga_adk@interflext.com>
2022-09-03 21:46:22 +0530

    Initial Commit

---
+++ Bike_station/Bike_stationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bike_stationApp.swift
+//  Bike_station
+//
+//  Created by vinoj randika on 2022-09-03.
+//
+
+import SwiftUI
+
+@main
+struct Bike_stationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bike_station/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bike_station
+//
+//  Created by vinoj randika on 2022-09-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
